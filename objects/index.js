9465ee6a62034c2ecdc2de1be5376ee4f4616f5f
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-05-31 10:41:52 -0400

    adds objects

---
+++ objects/index.js
@@ -0,0 +1,19 @@
+/*const game = {
+  title: 'guess the number',
+  biggestNum: 100,
+  play: function() {
+    const randomNumber = Math.floor(Math.random() 
+        * (this.biggestNum - this.smallestNum + 1)
+        + this.smallestNum);
+    this.secretNum = randomNumber;
+  }
+    )
+  }
+}
+const person = {
+    ... name: 'chris',
+    ... hat: 'yellow',
+    ...
+}
+person.name;*/
+
